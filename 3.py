e6ba9e546501adfc793f2615586293347048f863
yaer5hov <151647972+yaer5hov@users.noreply.github.com>
2024-02-03 15:35:18 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,6 +2,11 @@ import csv
 with open('scientist.csv', encouding = 'utf - 8') as file:
     header = list(csv.DictReader(file, delimiter = ',', quotechar = '"'))
     data = sorted(header, key, lambda x: x['ScientistName'])
+    ''' Описание файла
+    header - считываем файл и записываем значение через запятую
+    data = сортирум header по ScientistName
+    '''
+        
 id_project = input()
 while id_project != 'эксперимент':
     id_project = int(id_project)
