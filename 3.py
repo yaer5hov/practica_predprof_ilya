2711eb3e53ef4f376626f1b8c95bba334fb32a1f
yaer5hov <151647972+yaer5hov@users.noreply.github.com>
2024-02-03 15:31:51 +0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,14 @@
+import csv
+with open('scientist.csv', encouding = 'utf - 8') as file:
+    header = list(csv.DictReader(file, delimiter = ',', quotechar = '"'))
+    data = sorted(header, key, lambda x: x['ScientistName'])
+id_project = input()
+while id_project != 'эксперимент':
+    id_project = int(id_project)
+    for el in data:
+        if int(el['ScientistName']) == id_project:
+            name, surname,otchectvo = el['ScientistName'].split()
+            print(f'Ученый {{surname}.{name[0]}.{otchectvo[0]}} создал препарат: {preparation} - {date}')
+        else:
+            print('В этот день ученые отдыхали')
+    id_project = input()
